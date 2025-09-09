4792ce2b672abd9c3ca79ddac3a8a51558a9e99e
Ivan Dzhulynskyi <148357385+ivandzz@users.noreply.github.com>
2025-09-09 17:12:50 +0300

    Initial Commit

---
+++ Lab10/Lab10App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab10App.swift
+//  Lab10
+//
+//  Created by Іван Джулинський on 09.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Lab10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
